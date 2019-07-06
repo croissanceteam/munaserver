3e450a0909b71c390054780e1df37a865de6e097
LamaDev <hlama@live.fr>
2019-07-06 01:04:40 +0100

    Add War Aggregate and libs Mysql

---
+++ ODK Aggregate/create_db_and_user.sql
@@ -0,0 +1,5 @@
+create database `datawater`;
+        create user 'root'@'31.207.35.183' identified by '@Hornel91';
+        grant all on `datawater`.* to 'root'@'31.207.35.183' identified by '@Hornel91';
+        flush privileges;
+      

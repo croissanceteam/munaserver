0005a10184fc9f84977a43d1285202bfacb0392d
LamaDev <hlama@live.fr>
2019-09-12 18:03:13 +0100

    Add War Obs

--- ODK Aggregate/create_db_and_user.sql
+++
@@ -1,5 +0,0 @@
-create database `datawater`;
-        create user 'root'@'31.207.35.183' identified by '@Hornel91';
-        grant all on `datawater`.* to 'root'@'31.207.35.183' identified by '@Hornel91';
-        flush privileges;
-      
